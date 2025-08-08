b2bf7f779fdd8fc52da88c79570897e226c80525
santhoshvenkat <32671265+santhoshvenkat@users.noreply.github.com>
2025-08-08 15:43:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  },
   plugins: [react()],
-  // This is important for GitHub Pages deployment
-  base: './', 
+  // Set the base path for GitHub Pages deployment.
+  // This should be the name of your repository.
+  base: '/webapp/',
 })

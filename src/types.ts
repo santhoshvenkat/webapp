0e2babf5143a53bba8db0c917e1803834954cc3a
santhoshvenkat <32671265+santhoshvenkat@users.noreply.github.com>
2025-08-08 16:31:39 +0530

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export enum OrientationType {
+  PORTRAIT_PRIMARY = 'portrait-primary',
+  LANDSCAPE_PRIMARY = 'landscape-primary',
+  PORTRAIT_SECONDARY = 'portrait-secondary',
+  LANDSCAPE_SECONDARY = 'landscape-secondary',
+  UNKNOWN = 'unknown',
+}
+
+export interface WeatherData {
+  cityName: string;
+  temperature: number;
+  description: string;
+  icon: 'Clear' | 'Clouds' | 'Rain' | 'Snow' | 'Thunderstorm' | 'Drizzle' | 'Atmosphere' | string;
+}
